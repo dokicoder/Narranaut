525410d88df0021f30cda5b6fb328d686d64ba09
Kay Illner <kay.illner@googlemail.com>
2020-09-05 11:04:28 +0200

    add immer.js, alter error when .env is missing

--- client/src/firebase.ts
+++ client/src/firebase.ts
@@ -6,13 +6,13 @@ import React from 'react';
 export type User = firebase.User;
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_WEB_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_WEB_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_FIREBASE_WEB_DATABASE_URL,
-  projectId: process.env.REACT_APP_FIREBASE_WEB_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_WEB_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_WEB_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_WEB_ID,
+  apiKey: process.env?.REACT_APP_FIREBASE_WEB_API_KEY,
+  authDomain: process.env?.REACT_APP_FIREBASE_WEB_AUTH_DOMAIN,
+  databaseURL: process.env?.REACT_APP_FIREBASE_WEB_DATABASE_URL,
+  projectId: process.env?.REACT_APP_FIREBASE_WEB_PROJECT_ID,
+  storageBucket: process.env?.REACT_APP_FIREBASE_WEB_STORAGE_BUCKET,
+  messagingSenderId: process.env?.REACT_APP_FIREBASE_WEB_MESSAGING_SENDER_ID,
+  appId: process.env?.REACT_APP_FIREBASE_WEB_ID,
 };
 
 if (!firebaseConfig.apiKey) {
